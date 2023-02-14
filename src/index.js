7da17ffa789bdc3a311d024a9f40f30f765e42d7
Terry Ildefonso <master_correo144@hotmail.com>
2023-02-14 11:20:00 -0500

    Se borraron algunas cosas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import "./index.css";
 import Ableton from "./assets/ableton.jpg";
-import IMG_2 from "./assets/img_2.jpg";
-import IMG_3 from "./assets/img_3.jpg";
 
-document.getElementById("ableton").src = Ableton;
-document.getElementById("img_2").src = IMG_2;
-document.getElementById("img_3").src = IMG_3;
+// document.getElementById("ableton").src = Ableton;
