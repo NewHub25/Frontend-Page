d22ef4a75a96d9ab8a0c91271faadf8325eb843f
Terry Ildefonso <master_correo144@hotmail.com>
2023-02-14 10:20:03 -0500

    Se creo una nueva carpeta

---
+++ README.md
@@ -0,0 +1,4 @@
+hola mundo
+hola mundo hermoso
+Hello everyone, here is my first web of the month
+Please, share this document if you've enjoyed it
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import Ableton from "./assets/ableton.jpg";
+import IMG_2 from "./assets/img_2.jpg";
+import IMG_3 from "./assets/img_3.jpg";
+
+document.getElementById("ableton").src = Ableton;
+document.getElementById("img_2").src = IMG_2;
+document.getElementById("img_3").src = IMG_3;
